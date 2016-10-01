6bb149c6c8f83b0c51b87c60dbc7a5e878240356
ecmadao <wlec@outlook.com>
2016-10-01 08:53:53 +0800

    work with backend

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,35 @@
+var csrf = '';
+
+function getCsrf() {
+  $.ajax({
+    url: 'http://localhost:5000/csrf',
+    method: 'get',
+    success: function(data) {
+      console.log(data);
+      csrf = data.data;
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+}
+
+getCsrf();
+
 document.getElementById('info_submit').onclick = function() {
-  alert('info_submit');
+  $.ajax({
+    url: 'http://localhost:5000/user/signup',
+    method: 'post',
+    data: {
+      "email": 'wlec@outlook.com',
+      "password": '123456789',
+      "_csrf": csrf
+    },
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
 }

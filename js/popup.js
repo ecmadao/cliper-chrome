d3dfa725d73f4c4290c7ba433c86d1c4a0617dc8
ecmadao <wlec@outlook.com>
2016-10-01 10:18:48 +0800

    popup info

--- js/popup.js
+++ js/popup.js
@@ -25,8 +25,8 @@ function getCsrf() {
     url: 'http://localhost:5000/csrf',
     method: 'get',
     success: function(data) {
-      console.log(data);
       csrf = data.data;
+      console.log('csrf: ', csrf);
     },
     error: function(err) {
       console.log(err);
@@ -44,7 +44,31 @@ function signUp(email, password) {
       "_csrf": csrf
     },
     success: function(data) {
-      console.log(data);
+      toggleLoading(false);
+      if (data.success) {
+        var user = data.data;
+        chrome.storage.local.set({user: user}, function(result) {});
+        setPopDOM(user);
+      }
+    },
+    error: function(err) {
+      toggleLoading(false);
+      console.log(err);
+    }
+  });
+}
+
+function logout() {
+  $.ajax({
+    url: 'http://localhost:5000/user/logout',
+    method: 'post',
+    data: {
+      "_csrf": csrf
+    },
+    success: function(data) {
+      if (data.success) {
+        chrome.storage.local.set('user', null);
+      }
     },
     error: function(err) {
       console.log(err);
@@ -52,12 +76,47 @@ function signUp(email, password) {
   });
 }
 
+function toggleLoading(loading) {
+  var cliperContainer = document.querySelector('.cliper_container');
+  if (loading) {
+    cliperContainer.className = 'cliper_container active';
+  } else {
+    cliperContainer.className = 'cliper_container';
+  }
+}
+
+function setPopDOM(user) {
+  var userform = document.getElementById('cliper_userform');
+  var userinfo = document.getElementById('cliper_userinfo');
+  if (user && user.username) {
+    userform.className = "disabled";
+    userinfo.className = "active";
+    document.querySelector('.user_name').text = user.username;
+  } else {
+    userform.className = "active";
+    userinfo.className = "disabled";
+  }
+  toggleLoading(false);
+}
+
+toggleLoading(true);
 getCsrf();
 
-document.getElementById('info_submit').onclick = function() {
+// login or signup
+document.getElementById('cliper_login').onclick = function() {
   var email = getValidateEmail('cliper_email');
   var password = getValidatePassword('cliper_password');
   if (email && password) {
+    toggleLoading(true);
     signUp(email, password);
   }
 }
+// logout
+document.getElementById('cliper_logout').onclick = function() {
+  logout();
+}
+
+chrome.storage.sync.get('user', function(user) {
+  console.log(user);
+  setPopDOM(user);
+});

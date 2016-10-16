6cffde95b8aff0046344488c4e3d03ef5a1e0fe6
ecmadao <wlec@outlook.com>
2016-10-16 15:48:27 +0800

    load content scripts

--- src/entries/background.js
+++ src/entries/background.js
@@ -2,3 +2,11 @@ import Background from '../js/background';
 
 const background = new Background();
 background.initial();
+
+(() => {
+	chrome.tabs.query({currentWindow: true, active : false}, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.tabs.reload(tab.id);
+    });
+	});
+})();
